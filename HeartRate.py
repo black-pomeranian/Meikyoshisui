60c735f8f12c1f8fc091857f994de575357222f3
ryoneda <ryo.roma.16@gmail.com>
2022-08-13 15:21:04 +0900

    calcurate mean

--- HeartRate.py
+++ HeartRate.py
@@ -7,6 +7,7 @@ class HeartRateMonitor:
     INTERRUPT_PIN = 12
     MAX_DETECTED_TIMES_COUNT = 20
     MAX_PULSE_INTERVAL = 2.0
+    MAX_INTERVALS_COUNT = 10
  
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
@@ -16,36 +17,41 @@ class HeartRateMonitor:
  
     def _init_array(self):
         self.detected_times = deque([])
- 
-    def _calc_heart_rate(self):
-        return 1200.0 / (self.detected_times[-1] - self.detected_times[0])
- 
-    def _interrupt(self):
+        self.intervals = deque([])
+    
+    def _calc_HeartRate_mean(self):
         self.detected_times.append(time.time())
+        interval_mean = None
  
         if(len(self.detected_times) == 1):
             return
  
         interval = self.detected_times[-1] - self.detected_times[-2]
-        heart_rate = -1
+        self.intervals.append(interval)
+        
         if interval > self.MAX_PULSE_INTERVAL:
             print('Heart rate measure error. Monitoring will restart!')
             self._init_array()
             return
  
         if(len(self.detected_times) >= self.MAX_DETECTED_TIMES_COUNT):
-            heart_rate = self._calc_heart_rate()
             self.detected_times.popleft()
- 
-        print("HeartRate: {heart_rate}, Interval: {interval}".format(heart_rate = heart_rate, interval = interval))
- 
-    def execute(self):
+            
+        if(len(self.intervals) >= self.MAX_INTERVALS_COUNT):
+            interval_mean = sum(self.intervals) / len(self.intervals) 
+            self.intervals.popleft()
+        
+        return interval_mean
+            
+    def getHeartRate(self):
         print('Please ready your heart rate monitor.')
         time.sleep(3)
- 
+        
         while True:
             GPIO.wait_for_edge(self.INTERRUPT_PIN, GPIO.RISING)
-            self._interrupt()
+            return self._calc_HeartRate_mean()
+            
+        
  
 if __name__ == '__main__':
     monitor = HeartRateMonitor()
